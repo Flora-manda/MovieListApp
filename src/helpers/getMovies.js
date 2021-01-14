c376ad91cf6dacb805eac931aefd8910c8c30f13
Flora Uwadiegwu <flora.uwadiegwu@gmail.com>
2021-01-14 11:30:02 -0500

    Update https

--- src/helpers/getMovies.js
+++ src/helpers/getMovies.js
@@ -1,6 +1,6 @@
 export const getMovies = async function (searchTerm) {
   const apiKey = "6394ca94";
-  const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`;
+  const url = `https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`;
 
   return fetch(url, {
     method: "GET",
